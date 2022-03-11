c3d4ded9e696abf82fd837f39f979ce30003f7e8
LABDM-006$ <LABDM-006$@LABDM-006.laboratorio.ifms.br>
2022-03-11 19:33:37 -0400

    atualização

--- GA/src/Calculator/M10DAY_ATTR.java
+++ GA/src/Calculator/M10DAY_ATTR.java
@@ -5,17 +5,22 @@ import java.util.ArrayList;
 import Models.Data;
 
 public class M10DAY_ATTR implements SoftConstraints{
-	
-	private M10DAY_ATTR next;
+
+	private SoftConstraints next;
 
 	@Override
 	public int calculator(ArrayList<Data> timetable) {
+		// TODO Auto-generated method stub
 		return 3 + next.calculator(timetable);
 	}
 
 	@Override
 	public void setNext(SoftConstraints next) {
-		this.next = (M10DAY_ATTR) next;		
+		this.next = next;
+		
 	}
+
+
+	
 
 }
--- GA/src/Calculator/M4SHIFT_ATTR.java
+++ GA/src/Calculator/M4SHIFT_ATTR.java
@@ -6,18 +6,21 @@ import Models.Data;
 
 public class M4SHIFT_ATTR implements SoftConstraints {
 
+	private SoftConstraints next;
 
- private M4SHIFT_ATTR next;
+	@Override
+	public int calculator(ArrayList<Data> timetable) {
+		// TODO Auto-generated method stub
+		return 2 +  next.calculator(timetable);
+	}
 
-@Override
-public int calculator(ArrayList<Data> timetable) {
-	return 7 + next.calculator(timetable);
-}
+	@Override
+	public void setNext(SoftConstraints next) {
+		this.next = next;
+		
+	}
 
-@Override
-public void setNext(SoftConstraints next) {
-	this.next = (M4SHIFT_ATTR) next;		
-}
+	
 
 
 
